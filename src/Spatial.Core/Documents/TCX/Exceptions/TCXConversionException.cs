e56587dc334958255d2390af244aad00285b343e
Joe Walters <info@thenakeddeveloper.com>
2025-10-05 21:04:20 +0100

    Removed unneeded constructors

--- src/Spatial.Core/Documents/TCX/Exceptions/TCXConversionException.cs
+++ src/Spatial.Core/Documents/TCX/Exceptions/TCXConversionException.cs
@@ -6,8 +6,6 @@ namespace Spatial.Core.Documents.TCX.Exceptions
 {
     public class TCXConversionException : Exception
     {
-        public TCXConversionException() : base() { }
         public TCXConversionException(String message) : base(message) { }
-        public TCXConversionException(String message, Exception inner) : base(message, inner) { }
     }
 }
